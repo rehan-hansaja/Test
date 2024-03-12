f64c758e5c74e46440c79d17250997eb7ecccdec
Linal Mathila Bandara <linalmathilab@gmail.com>
2024-03-12 23:33:00 +0530

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:camera/SplashScreen.dart';
-import 'package:camera/detected_page.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:convert';
